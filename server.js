b8bb3727af847b16496d5c531afbaad9416ad3f5
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2023-03-09 13:51:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3030;
 const Article = require('./Db/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
