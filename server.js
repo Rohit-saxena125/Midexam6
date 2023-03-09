b6e1cef1a3c938767ac5b24e9f5d1452d2a3c2f6
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2023-03-09 14:38:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-require('dot-env');
+require('dotenv').config();
 const PORT = process.env.PORT || 3030;
 const Article = require('./Db/article')
 const articleRouter = require('./routes/articles')
